71b39a061c3de0efa2077c8ca274c979f071f447
hackerhihello <hackerhihello123@gmail.com>
2024-12-17 16:10:45 +0530

    updated the mangodbatlas

--- swagger.js
+++ swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/api',
+        url: 'https://labsserversideproject.onrender.com',
       },
     ],
     components: {
