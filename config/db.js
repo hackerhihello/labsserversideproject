44a4ba1d5224e7523854da386898e2145374393b
hackerhihello <hackerhihello123@gmail.com>
2024-12-17 16:24:12 +0530

    updated the mangodbatlas

--- config/db.js
+++ config/db.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useNewUrlParser: true,  // This is still useful for new URL parser
     });
     console.log('MongoDB connected successfully');
   } catch (err) {
